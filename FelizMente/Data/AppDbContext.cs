4c8b5d9868788196ab51c9752d274bcdb9fdb5e5
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-10-11 16:28:28 -0300

    Update AppDbContext.cs

--- FelizMente/Data/AppDbContext.cs
+++ FelizMente/Data/AppDbContext.cs
@@ -41,7 +41,7 @@ namespace FelizMente.Data
                 //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
                 }
             }
 
@@ -54,7 +54,7 @@ namespace FelizMente.Data
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
                 }
             }
 
